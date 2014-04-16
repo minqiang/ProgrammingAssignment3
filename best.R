a4f7fac13f035fd9469bf6c2979210069acdd5b3
Minqiang Li <minqiang.li@gmail.com>
2014-04-16 07:42:03 -0400

    init commit best.R, still buggy

---
+++ best.R
@@ -0,0 +1,28 @@
+best <- function(state, outcome) {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+  data <- read.csv("outcome-of-care-measures.csv", 
+                   as.is=T)
+  
+  state.data <- subset(data, State==state)
+  names <- state.data$Hospital.Name
+  
+  if (outcome=="heart attack") {
+    y <- state.data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack    
+  } else if (outcome == "heart failure") {
+    y <- state.data$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
+  } else if (outcome == "pneumonia") {
+    y <- state.data$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia
+  }
+  
+  y <- suppressWarnings(as.numeric(y))
+  
+  clean.data <- data.frame( names=names, y=y)
+  clean.data <- clean.data[complete.cases(clean.data),]
+  ord <- order(clean.data[,2], clean.data[,1])
+  clean.data <- clean.data[ord,]
+  
+  clean.data$names[[1]]
+}
