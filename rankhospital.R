93a0e993e36405a2907569e5eae8cdddf81c6824
Di Li <dili_china@hotmail.com>
2014-04-18 00:24:01 -0400

    added file rankhospital.R. Fixed a bug in best.R with invalid outcome input.

--- rankhospital.R
+++ rankhospital.R
@@ -1,14 +1,15 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
   options(stringsAsFactors = FALSE)
   ## Read outcome data
   ## Check that state and outcome are valid
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
-  data <- read.csv("outcome-of-care-measures.csv", as.is=T)
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+  
+  data <- read.csv("outcome-of-care-measures.csv", 
+                   as.is=T)
   
   state.data <- subset(data, State==state)
   names <- state.data$Hospital.Name
-  
   if (length(names)==0)
     stop("invalid state")
   
@@ -24,10 +25,21 @@ best <- function(state, outcome) {
   y <- suppressWarnings(as.numeric(y))
   
   clean.data <- data.frame( names=names, y=y, stringAsFactors=F)
-  
   clean.data <- clean.data[complete.cases(clean.data),]
   ord <- order(clean.data[,2], clean.data[,1])
+
   clean.data <- clean.data[ord,]
-  clean.data$names[[1]]
+  
+  if(num=="best")
+    return(clean.data$names[[1]])
+  else  if(num=="worst") {
+    return(tail(clean.data$names, 1))
+  }
+  else if (num >0 & num < length(clean.data$names)) {
+    #print(clean.data)
+    return(clean.data$names[[num]])
+  }
+  else 
+    return(NA)
   
 }
